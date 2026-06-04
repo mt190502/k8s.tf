9014bafc204cf059a100d660913802eaedde54cd
Taha <mt190502@mtaha.dev>
2026-06-04 23:55:31 +0300

    refactor(manifests/apps): decrease wal_keep_size for single-instance postgres pods

--- modules/manifests/apps/miniflux/db.tf
+++ modules/manifests/apps/miniflux/db.tf
@@ -8,6 +8,34 @@
 #  Connection string format:                                                                      #
 #    postgresql://{owner}:{password}@{app}-postgres-rw.{namespace}.svc.cluster.local:5432/{db}    #
 ## ============================================================================================= ##
+locals {
+  cnpg_default_params = {
+    archive_mode               = "on"
+    archive_timeout            = "5min"
+    dynamic_shared_memory_type = "posix"
+    full_page_writes           = "on"
+    log_destination            = "csvlog"
+    log_directory              = "/controller/log"
+    log_filename               = "postgres"
+    log_rotation_age           = "0"
+    log_rotation_size          = "0"
+    log_truncate_on_rotation   = "false"
+    logging_collector          = "on"
+    max_parallel_workers       = "32"
+    max_replication_slots      = "32"
+    max_worker_processes       = "32"
+    shared_memory_type         = "mmap"
+    shared_preload_libraries   = ""
+    ssl_max_protocol_version   = "TLSv1.3"
+    ssl_min_protocol_version   = "TLSv1.3"
+    wal_keep_size              = "64MB"
+    wal_level                  = "logical"
+    wal_log_hints              = "on"
+    wal_receiver_timeout       = "5s"
+    wal_sender_timeout         = "5s"
+  }
+}
+
 resource "kubernetes_secret_v1" "postgres" {
   count = var.enabled ? 1 : 0
   metadata {
@@ -35,6 +63,9 @@ resource "kubernetes_manifest" "postgres" {
     spec = {
       instances             = var.config.pg.replicas
       primaryUpdateStrategy = "unsupervised"
+      postgresql = {
+        parameters = local.cnpg_default_params
+      }
       bootstrap = {
         initdb = {
           database = var.config.name
